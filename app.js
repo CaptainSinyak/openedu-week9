4a413e7e382d6e446076ccddfcf858cb164e0eba
CaptainSinyak <antonioe15000@gmail.com>
2020-12-20 12:12:37 +0300

    commit on master

--- app.js
+++ app.js
@@ -1,39 +1,48 @@
-export default (express, puppeteer) => {
+export default (express, puppeteer, Zombie) => {
     
     const app = express();
     const author = 'itmo288485'
 
-    app.use(function (req, res, next) {
+    app
+    .use(function (req, res, next) {
         res.setHeader('Content-Type', 'text/plain')
         res.setHeader("Access-Control-Allow-Origin", "*");
         res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
         next()
-    });
+    })
 
-    app.get('/login/', (req, res) => {
+    .get('/login/', (req, res) => {
         res.send(author)
     })
-
-    app.get('/test/', async (req, res) => {
-        const { URL } = req.query
-        const browser = await puppeteer.launch({
-            headless: true,
-            args: [
-                '--no-sandbox',
-                '--disable-setuid-sandbox',
-            ],
-        })
-        const page = await browser.newPage()
-        await page.goto(URL)
-        await page.click('#bt')
-        const value = await page.evaluate(async () => {
-            const input = document.getElementById('inp')
-            return input.value
-        })
-        res.send(value)
+    .get('/test/', async r => {
+        const { URL } = r.query;
+        const page = new Zombie();
+        await page.visit(URL);
+        await page.pressButton('#bt');
+        const got = await page.document.querySelector('#inp').value;
+        r.res.send(got);
     })
+    
+    // .get('/test/', async (req, res) => {
+    //     const { URL } = req.query
+    //     const browser = await puppeteer.launch({
+    //         headless: true,
+    //         args: [
+    //             '--no-sandbox',
+    //             '--disable-setuid-sandbox',
+    //         ],
+    //     })
+    //     const page = await browser.newPage()
+    //     await page.goto(URL)
+    //     await page.click('#bt')
+    //     const value = await page.evaluate(async () => {
+    //         const input = document.getElementById('inp')
+    //         return input.value
+    //     })
+    //     res.send(value)
+    // })
 
-    app.all('*', (req, res) => {
+    .all('*', (req, res) => {
         res.send(author)
     })
 
